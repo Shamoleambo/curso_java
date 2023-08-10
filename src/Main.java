5000e5e4339bc02224b26da7ae3a9a8db3747bd9
Tiago Gomes <tidgomes@gmail.com>
2023-08-10 18:05:06 -0300

    use nextLine and work around the reading data buffer

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,21 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		String name;
-		int age;
-		double moneyInTheBank;
-		char gender;
+		int number;
+		String text1, text2, text3;
+
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("What is your name?");
-		name = sc.next();
-		System.out.println("What is your age?");
-		age = sc.nextInt();
-		System.out.println("How much money do you have in your bank account?");
-		moneyInTheBank = sc.nextDouble();
-		System.out.println("What is your gender?(M/F)");
-		gender = sc.next().charAt(0);
+		System.out.println("Provide a number and three strings");
+		number = sc.nextInt();
+		sc.nextLine();
+		text1 = sc.nextLine();
+		text2 = sc.nextLine();
+		text3 = sc.nextLine();
 
-		System.out.printf("%nYour name is %s%n", name);
-		System.out.printf("You are %d years old%n", age);
-		System.out.printf("You have R$ %.2f saved in your bank account%n", moneyInTheBank);
-		System.out.printf("And your gender is %s", gender);
+		System.out.printf("%n%d %n%s %n%s %n%s", number, text1, text2, text3);
+
 		sc.close();
-
 	}
 
 }
