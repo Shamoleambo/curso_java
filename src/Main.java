02acb5d027c6426600e3fd9d308db229d1bddd0e
Tiago Gomes <tidgomes@gmail.com>
2023-08-10 14:11:39 -0300

    test printf function

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		double floatVariable = 233.0/4;
+		String name = "Mano";
+		
+		System.out.printf("%.4f%n", floatVariable);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f%n", floatVariable);
+		
+		System.out.printf("%s has %.2f dollars%n", name, floatVariable);
 
 	}
 
