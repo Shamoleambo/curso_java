5c5bf21530877608d9977c9ae9e645ca5058eb6d
Tiago Gomes <tidgomes@gmail.com>
2023-08-16 15:51:29 -0300

    first while control structure

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,14 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int number;
+		int x;
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Provide an integer number: ");
-		number = sc.nextInt();
+		x = sc.nextInt();
 		
-		if(number < 0 ) {
-			System.out.printf("%d is your number, and is SMALLER than 0", number);
-		}else if(number > 0){
-			System.out.printf("%d is your number, and is BIGGER than 0", number);
-		} else {
-			System.out.println("Your number IS 0");
+		while(x != 0) {
+			x = sc.nextInt();
 		}
 		
 		sc.close();
