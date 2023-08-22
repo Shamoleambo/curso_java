475e401eb1a6b218f97876bdcc42457ff1404124
Tiago Gomes <tidgomes@gmail.com>
2023-08-22 09:41:38 -0300

    learn for each

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,11 @@
-import java.util.Locale;
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+		String[] names = new String[] {"Mano", "Truta", "Tiu"};
 		
-		System.out.println("What's the array size?");
-		int arraySize = sc.nextInt();
-		
-		double[] heightArray = new double[arraySize];
-		
-		for(int i = 0; i<arraySize; i++) {
-			heightArray[i] = sc.nextDouble();
+		for(String name : names) {
+			System.out.println(name);
 		}
-		
-		System.out.println(average(heightArray));
-		
-		sc.close();
-	}
-	
-	public static double average(double[] array) {
-		int arraySize = array.length;
-		double sum = 0;
-		for(int i=0; i<arraySize; i++) {
-			sum += array[i];
-		}
-		
-		return sum/arraySize;
 	}
 
 }
