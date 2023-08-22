be8828f6c2b41948db67d9cb20ecdaee7520cee7
Tiago Gomes <tidgomes@gmail.com>
2023-08-22 12:12:59 -0300

    learn different date types

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,40 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
 	public static void main(String[] args) {
-		List<String> names = new ArrayList<String>();
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
-		names.add("Mano");
-		names.add("Truta");
-		names.add("Tiu");
-		names.add(0, "Robert");
+		LocalDate d01 = LocalDate.now();
+		LocalDateTime d02 = LocalDateTime.now();
+		Instant d03 = Instant.now();
 		
-		names.remove("Robert");
-		names.remove(0);
-		names.removeIf(name -> name.charAt(0) == 'T');
+		LocalDate d04 = LocalDate.parse("2023-08-22");
+		LocalDateTime d05 = LocalDateTime.parse("2023-08-22T12:00:01");
+		Instant d06 = Instant.parse("2023-08-22T12:00:00Z");
+		Instant d07 = Instant.parse("2023-08-22T12:00:00-03:00");
 		
-		for(String name : names) {
-			System.out.println(name);
-		}
-		System.out.println(names.size());
+		LocalDate d08 = LocalDate.parse("22/08/2023", fmt1);
+		LocalDateTime d09 = LocalDateTime.parse("22/08/2023 12:12", fmt2);
 		
-		names.add("Vito");
-		names.add("Michael");
-		names.add("Sonny");
-		names.add("Fredo");
+		LocalDate d10 = LocalDate.of(2023, 8, 22);
+		LocalDateTime d11 = LocalDateTime.of(2023, 8, 22, 12, 0);
 		
-		List<String> godfathers = names.stream().filter(name -> name.charAt(1) == 'i').collect(Collectors.toList());
-		
-		for(String name : names) {
-			System.out.println(name);
-		}
-		for(String godfather : godfathers) {
-			System.out.printf("Godfather: %s%n", godfather);
-		}
-		System.out.println(names.indexOf("Michael"));
-		System.out.println(names.indexOf("Henry"));
-		
-		String firstGodfather = names.stream().filter(name -> name.charAt(0) == 'V').findFirst().orElse(null);
-		System.out.println(firstGodfather);
+		System.out.println("d01 = " + d01);
+		System.out.println("d02 = " + d02);
+		System.out.println("d03 = " + d03);
+		System.out.println("d04 = " + d04);
+		System.out.println("d05 = " + d05);
+		System.out.println("d06 = " + d06);
+		System.out.println("d07 = " + d07);
+		System.out.println("d08 = " + d08);
+		System.out.println("d09 = " + d09);
+		System.out.println("d10 = " + d10);
+		System.out.println("d11 = " + d11);
 	}
 
 }
