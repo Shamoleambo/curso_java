4e5831b7934802ffe827f013cc943ef6a1ed43e7
Tiago Gomes <tidgomes@gmail.com>
2023-08-22 09:53:23 -0300

    learn Lists

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		String[] names = new String[] {"Mano", "Truta", "Tiu"};
+		List<String> names = new ArrayList<String>();
+		
+		names.add("Mano");
+		names.add("Truta");
+		names.add("Tiu");
 		
 		for(String name : names) {
 			System.out.println(name);
