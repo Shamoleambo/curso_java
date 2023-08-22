3b44d4d9a9eb8f57d9c47133c792b148d7a9e797
Tiago Gomes <tidgomes@gmail.com>
2023-08-22 10:12:07 -0300

    explore add, remove, filter, indexOf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -9,10 +10,35 @@ public class Main {
 		names.add("Mano");
 		names.add("Truta");
 		names.add("Tiu");
+		names.add(0, "Robert");
+		
+		names.remove("Robert");
+		names.remove(0);
+		names.removeIf(name -> name.charAt(0) == 'T');
 		
 		for(String name : names) {
 			System.out.println(name);
 		}
+		System.out.println(names.size());
+		
+		names.add("Vito");
+		names.add("Michael");
+		names.add("Sonny");
+		names.add("Fredo");
+		
+		List<String> godfathers = names.stream().filter(name -> name.charAt(1) == 'i').collect(Collectors.toList());
+		
+		for(String name : names) {
+			System.out.println(name);
+		}
+		for(String godfather : godfathers) {
+			System.out.printf("Godfather: %s%n", godfather);
+		}
+		System.out.println(names.indexOf("Michael"));
+		System.out.println(names.indexOf("Henry"));
+		
+		String firstGodfather = names.stream().filter(name -> name.charAt(0) == 'V').findFirst().orElse(null);
+		System.out.println(firstGodfather);
 	}
 
 }
