3f263ed10538c96b39886b9cf8a3c9a9c91ec42a
Tiago Gomes <tidgomes@gmail.com>
2023-08-23 11:40:18 -0300

    learn about enums

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,11 @@
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class Main {
 
 	public static void main(String[] args) {
-		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		Status status1 = Status.AWESOME;
+		Status status2 = Status.valueOf("BAD");
 		
-		LocalDate d01 = LocalDate.parse("2023-08-22");
-		LocalDateTime d02 = LocalDateTime.parse("2023-08-22T12:12:00");
-		
-		System.out.println("d01 = " + d01.format(fmt1));
-		System.out.println("d01 = " + fmt1.format(d01));
-		System.out.println("d02 = " + d02.format(fmt2));
-		System.out.println("d02 = " + fmt2.format(d02));
+		System.out.println(status1);
+		System.out.println(status2);
 	}
 
 }
---
+++ src/Status.java
@@ -0,0 +1,11 @@
+
+public enum Status {
+
+	AWESOME,
+	VERY_GOOD,
+	NICE,
+	OK,
+	NOT_SO_GOOD,
+	BAD,
+	AWFUL
+}
