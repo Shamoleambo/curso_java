d7d1d663f5a757b1010b708dc58fa212fe5f4ae7
Tiago Gomes <tidgomes@gmail.com>
2023-08-28 09:41:53 -0300

    reminder for polymorphism

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,11 @@ public class Main {
 	public static void main(String[] args) {
 		Account acc1 = new BusinessAccount("Mano", 1234, 1000.00, 10000.00);
 		Account acc2 = new SavingsAccount("Truta", 1235, 1000.0, 0.01);
-		
-		if(acc1 instanceof BusinessAccount) {
-			BusinessAccount acc3 = (BusinessAccount) acc1;
-			acc3.loan(500.00);
-			System.out.println(acc3.getBalance());
-		}
-		if (acc2 instanceof SavingsAccount) {
-			SavingsAccount acc4 = (SavingsAccount) acc2;
-			acc4.update();
-			System.out.println(acc4.getBalance());
-		}
+
+		acc1.withdraw(100.0);
+		acc2.withdraw(100.0);
+		System.out.println(acc1.getBalance());
+		System.out.println(acc2.getBalance());
 	}
 
 }
