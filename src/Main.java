da16ebb4906676357974af0d6ab52d50dff4c2ff
Tiago Gomes <tidgomes@gmail.com>
2023-08-29 12:24:40 -0300

    reading files

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,23 @@
-import java.util.InputMismatchException;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		method1();
-		System.out.println("Program end");
-	}
-
-	public static void method1() {
-		System.out.println("***Method 1 START***");
-		method2();
-		System.out.println("***Method 1 END***");
-	}
-
-	public static void method2() {
-		System.out.println("***Method 2 START***");
-		Scanner sc = new Scanner(System.in);
+		File file = new File("C:\\Users\\tidgo\\Documentos\\estudos\\java\\in");
+		Scanner sc = null;
 
 		try {
-			String[] vect = sc.nextLine().split(" ");
-			Integer postition = sc.nextInt();
-
-			System.out.println(vect[postition]);
-		} catch (InputMismatchException e) {
-			System.out.println("Invalid input for position, it must be an Integer!");
-		} catch (ArrayIndexOutOfBoundsException e) {
-			e.printStackTrace();
-			System.out.println("Position out of array range!");
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			if(sc != null) sc.close();
 		}
-
-		sc.close();
-		System.out.println("***Method 2 END***");
 	}
 
 }
