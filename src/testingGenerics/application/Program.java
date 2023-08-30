bc8112ce7ae6a6fb6c8f0c4c12201b5e5d6b8b82
Tiago Gomes <tidgomes@gmail.com>
2023-08-30 16:15:31 -0300

    refactor to use generics to make PrintService reusable

--- src/testingGenerics/application/Program.java
+++ src/testingGenerics/application/Program.java
@@ -6,23 +6,34 @@ import testingGenerics.services.PrintService;
 
 public class Program {
 	public static void main(String[] args) {
-		PrintService ps = new PrintService();
+		PrintService<Integer> psInt = new PrintService<>();
+		PrintService<String> psStr = new PrintService<>();
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("How many strings do you want to add? ");
+		System.out.print("How many values do you want to add? ");
 		Integer n = sc.nextInt();
 		sc.nextLine();
 
 		for (int i = 0; i < n; i++) {
+			System.out.printf("#%d => Type integer to be added: ", i + 1);
+			Integer value = sc.nextInt();
+			sc.nextLine();
+			psInt.addValue(value);
+
 			System.out.printf("#%d => Type string to be added: ", i + 1);
-			String value = sc.nextLine();
-			ps.addValue(value);
+			String str = sc.nextLine();
+			psStr.addValue(str);
 		}
 
 		sc.close();
 
-		System.out.println("First: " + ps.first());
-		ps.print();
+		System.out.println("First: " + psInt.first());
+		psInt.print();
+
+		System.out.println();
+
+		System.out.println("First: " + psStr.first());
+		psInt.print();
 	}
 }
--- src/testingGenerics/services/PrintService.java
+++ src/testingGenerics/services/PrintService.java
@@ -3,17 +3,17 @@ package testingGenerics.services;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PrintService {
-	private List<String> list = new ArrayList<>();
+public class PrintService<T> {
+	private List<T> list = new ArrayList<>();
 
-	public void addValue(String value) {
+	public void addValue(T value) {
 		list.add(value);
 	}
-	
-	public String first() {
+
+	public T first() {
 		return this.list.get(0);
 	}
-	
+
 	public void print() {
 		System.out.println(this.list.toString());
 	}
