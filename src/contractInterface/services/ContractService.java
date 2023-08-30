dd254d3dab2087dbcf7c309905c762ceb8cb3c82
Tiago Gomes <tidgomes@gmail.com>
2023-08-30 09:04:35 -0300

    create processContract method

---
+++ src/contractInterface/services/ContractService.java
@@ -0,0 +1,31 @@
+package contractInterface.services;
+
+import java.time.LocalDate;
+
+import contractInterface.entities.Contract;
+import contractInterface.entities.Installment;
+
+public class ContractService {
+	
+	OnlinePaymentService onlinePaymentService;
+	
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+
+	public void processContract(Contract contract, Integer months) {
+		Double nominalPayment = contract.getTotalAmount() / months;
+		
+		System.out.println("Installments:");
+		for(Integer month = 1; month <= months; month++) {
+			Double interest = nominalPayment + onlinePaymentService.interest(nominalPayment, month);
+			Double interestPlusFee = interest + onlinePaymentService.paymentFee(interest);
+			
+			LocalDate installmentDueDate = contract.getDate().plusMonths(month);
+			
+			Installment installment = new Installment(installmentDueDate, interestPlusFee);
+			
+			System.out.println(installment.getDueDate() + " - " + interestPlusFee);
+		}
+	}
+}
