8ea79ac6438ae8607e6c3e349bc5b416de1341c9
Tiago Gomes <tidgomes@gmail.com>
2023-08-31 11:51:03 -0300

    learn addAll, removeAll and retainAll operations

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,24 @@
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Set<String> hashSet = new HashSet<>();
-		hashSet.add("Truta");
-		hashSet.add("Mano");
-		hashSet.add("Tiu");
-		hashSet.add("Eita");
-		System.out.println(hashSet.contains("Mano"));
-		System.out.println(hashSet.contains("Maria"));
-		for(String name: hashSet) {
-			System.out.println(name);
-		}
+		Set<Integer> a = new TreeSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(2, 4, 6, 7, 9, 11, 23));
 		
-		System.out.println();
-		hashSet.remove("Mano");
-		hashSet.removeIf(name -> name.charAt(0) == 'T');
-		for(String name: hashSet) {
-			System.out.println(name);
-		}
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
+		
+		Set<Integer> d = new TreeSet<>(a);
+		d.removeAll(b);
+		System.out.println(d);
+		
+		Set<Integer> e = new TreeSet<>(a);
+		e.retainAll(b);
+		System.out.println(e);
 	}
 }
