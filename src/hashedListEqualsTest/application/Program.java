638e99022a2c7ba77636c4081addcffc87bbffd5
Tiago Gomes <tidgomes@gmail.com>
2023-08-31 14:31:13 -0300

    use compareTo to order a TreeSet

--- src/hashedListEqualsTest/application/Program.java
+++ src/hashedListEqualsTest/application/Program.java
@@ -2,6 +2,7 @@ package hashedListEqualsTest.application;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import compareToGenericsTest.entities.Product;
 
@@ -9,15 +10,19 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Set<Product> productsSet = new HashSet<>();
-		Product prod = new Product("Iphone", 4000.0);
+		Set<Product> productsSet = new TreeSet<>();
 		
-		productsSet.add(prod);
+		productsSet.add(new Product("Notebook", 3000.0));
 		productsSet.add(new Product("Iphone", 4000.00));
 		productsSet.add(new Product("Fridge", 1000.00));
 		
+		Product prod = new Product("Notebook", 3000.0);
 		
 		System.out.println(productsSet.contains(prod));
+		
+		for(Product p : productsSet) {
+			System.out.println(p);
+		}
 	}
 
 }
