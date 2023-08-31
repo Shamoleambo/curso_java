5021f2accda86482551c3625470dc03ff81e4476
Tiago Gomes <tidgomes@gmail.com>
2023-08-31 11:36:52 -0300

    learn Set, HashSet, TreeSet and LinkedHashSet

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,41 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
 
 	public static void main(String[] args) {
-		String mano = "Mano";
-		String truta = "Truta";
+		Set<String> hashSet = new HashSet<>();
+		hashSet.add("Truta");
+		hashSet.add("Mano");
+		hashSet.add("Tiu");
+		System.out.println(hashSet.contains("Mano"));
+		System.out.println(hashSet.contains("Maria"));
+		for(String name: hashSet) {
+			System.out.println(name);
+		}
 		
-		System.out.println(mano.equals(truta));
+		System.out.println();
+		Set<String> treehashSet = new TreeSet<>();
+		treehashSet.add("Truta");
+		treehashSet.add("Mano");
+		treehashSet.add("Tiu");
+		System.out.println(treehashSet.contains("Mano"));
+		System.out.println(treehashSet.contains("Maria"));
+		for(String name: treehashSet) {
+			System.out.println(name);
+		}
 		
-		System.out.println(mano.hashCode());
-		System.out.println(truta.hashCode());
+		System.out.println();
+		Set<String> lnkhasSet = new LinkedHashSet<>();
+		lnkhasSet.add("Mano");
+		lnkhasSet.add("Tiu");
+		lnkhasSet.add("Truta");
+		System.out.println(lnkhasSet.contains("Mano"));
+		System.out.println(lnkhasSet.contains("Maria"));
+		for(String name: lnkhasSet) {
+			System.out.println(name);
+		}
 	}
 }
