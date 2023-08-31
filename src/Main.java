66550ef57b71074d9789b6fd71e396aeda270ee3
Tiago Gomes <tidgomes@gmail.com>
2023-08-31 10:07:29 -0300

    learn hashCode and equals and its differences

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
 
 	public static void main(String[] args) {
-		List<Integer> intList = Arrays.asList(1,2,3);
-		List<Double> doubList = Arrays.asList(1.1, 2.3, 5.7);
-		List<Object> objList = new ArrayList<Object>();
+		String mano = "Mano";
+		String truta = "Truta";
 		
-		copy(intList, objList);
-		copy(doubList, objList);
+		System.out.println(mano.equals(truta));
 		
-		printList(objList);
-	}
-	
-	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
-		for(Number number: source) {
-			destiny.add(number);
-		}
-	}
-	
-	public static void printList(List<?> list) {
-		for(Object obj: list) {
-			System.out.println(obj);
-		}
+		System.out.println(mano.hashCode());
+		System.out.println(truta.hashCode());
 	}
 }
---
+++ src/hashCodeTesting/application/Program.java
@@ -0,0 +1,17 @@
+package hashCodeTesting.application;
+
+import hashCodeTesting.entities.Client;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Client c1 = new Client("Mano", "mano@mail.com");
+		Client c2 = new Client("Truta", "truta@mail.com");
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		
+		System.out.println(c1.equals(c2));
+	}
+
+}
