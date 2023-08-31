5e2ab18b36092b5f15958e675c7a209c6d4bedab
Tiago Gomes <tidgomes@gmail.com>
2023-08-31 09:28:33 -0300

    learn wild card type

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\tidgo\\Documentos\\estudos\\java\\in");
-		Scanner sc = null;
-
-		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
-			}
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		} finally {
-			if(sc != null) sc.close();
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+		List<String> names = Arrays.asList("Mano", "Truta", "Tiu");
+		
+		printList(list);
+		printList(names);
+	}
+	
+	public static void printList(List<?> list) {
+		for(Object item: list) {
+			System.out.println(item);
 		}
 	}
 
