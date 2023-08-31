f795c97c695240ae2b791bdf1794c3385c1725d0
Tiago Gomes <tidgomes@gmail.com>
2023-08-31 09:45:44 -0300

    learn the difference from wild card extends and super

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,28 @@
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-		List<String> names = Arrays.asList("Mano", "Truta", "Tiu");
+		List<Integer> intList = new ArrayList<Integer>();
+		intList.add(1);
+		intList.add(2);
+		intList.add(3);
 		
-		printList(list);
-		printList(names);
+		List<? extends Number> numList = intList;
+		System.out.println(numList.get(0));
+		//numList.add(4); ERROR
+		
+		List<Object> objList = new ArrayList<Object>();
+		objList.add("Mano");
+		objList.add("Truta");
+		objList.add("Tiu");
+		
+		List<? super Number> myNums = objList;
+		myNums.add(22);
+		myNums.add(33.33);
+		myNums.add(1L);
+		
+//		Number x = myNums.get(0); ERROR
 	}
-	
-	public static void printList(List<?> list) {
-		for(Object item: list) {
-			System.out.println(item);
-		}
-	}
-
 }
