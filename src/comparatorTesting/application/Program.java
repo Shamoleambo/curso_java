9ba999d9b6e911ca7c0c55ec5c365fa906373a77
Tiago Gomes <tidgomes@gmail.com>
2023-09-01 09:16:33 -0300

    use Comparator with a lambda expression

--- src/comparatorTesting/application/Program.java
+++ src/comparatorTesting/application/Program.java
@@ -9,23 +9,19 @@ import comparatorTesting.entities.Product;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		List<Product> products = new ArrayList<>();
-		
+
 		products.add(new Product("TV", 900.00));
 		products.add(new Product("Notebook", 1200.00));
 		products.add(new Product("Tablet", 450.00));
-		
-		Comparator<Product> comp = new Comparator<Product>() {
-			@Override
-			public int compare(Product product1, Product product2) {
-				return product1.getName().toUpperCase().compareTo(product2.getName().toUpperCase());
-			}
-		};
-		
-		products.sort(comp);;
-		
-		for(Product product: products) {
+
+		Comparator<Product> comp = (product1, product2) -> product1.getName().toUpperCase()
+				.compareTo(product2.getName().toUpperCase());
+
+		products.sort(comp);
+
+		for (Product product : products) {
 			System.out.println(product);
 		}
 
