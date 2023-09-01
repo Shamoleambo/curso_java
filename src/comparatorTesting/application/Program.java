b66940f22745c004d8cdaae528348873811cf89f
Tiago Gomes <tidgomes@gmail.com>
2023-09-01 09:00:16 -0300

    create program to sort products list

---
+++ src/comparatorTesting/application/Program.java
@@ -0,0 +1,27 @@
+package comparatorTesting.application;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import comparatorTesting.entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Product> products = new ArrayList<>();
+		
+		products.add(new Product("TV", 900.00));
+		products.add(new Product("Notebook", 1200.00));
+		products.add(new Product("Tablet", 450.00));
+		
+		Collections.sort(products);
+		
+		for(Product product: products) {
+			System.out.println(product);
+		}
+
+	}
+
+}
