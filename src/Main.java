373754ca1753efa76cb2902716a47125d5305870
Tiago Gomes <tidgomes@gmail.com>
2023-09-04 10:14:51 -0300

    learn how to create a stream

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		List<Integer> list = Arrays.asList(3,4,5,7,8,11);
 		
+		Stream<Integer> st1 = list.stream();
+		System.out.println(Arrays.toString(st1.toArray()));
 
 	}
 }
