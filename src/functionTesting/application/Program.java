c033d20479c767aa9bfdad52f6bdedeedd192d13
Tiago Gomes <tidgomes@gmail.com>
2023-09-04 09:48:09 -0300

    use inline lambda expression

--- src/functionTesting/application/Program.java
+++ src/functionTesting/application/Program.java
@@ -2,7 +2,6 @@ package functionTesting.application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import functionTesting.entities.Product;
@@ -10,19 +9,17 @@ import functionTesting.entities.Product;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		List<Product> products = new ArrayList<Product>();
-		
+
 		products.add(new Product("TV", 1200.0));
 		products.add(new Product("Notebook", 3000.0));
 		products.add(new Product("Smartphone", 4000.0));
 		products.add(new Product("Tablet", 2400.0));
-		
-		Function<Product, String> toUpperCase = (Product p) -> p.getName().toUpperCase();
-		
-		List<String> names = products.stream().map(toUpperCase).collect(Collectors.toList());
+
+		List<String> names = products.stream().map((p) -> p.getName().toUpperCase()).collect(Collectors.toList());
 		names.forEach(System.out::println);
-		
+
 	}
 
 }
