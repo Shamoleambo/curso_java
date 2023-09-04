61d6e1bda82b7c1606cbc83b8e1f9e93e7de1557
Tiago Gomes <tidgomes@gmail.com>
2023-09-04 09:47:12 -0300

    use a declared lambda expression to implements Function's apply method

--- src/functionTesting/application/Program.java
+++ src/functionTesting/application/Program.java
@@ -2,6 +2,7 @@ package functionTesting.application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import functionTesting.entities.Product;
@@ -17,7 +18,9 @@ public class Program {
 		products.add(new Product("Smartphone", 4000.0));
 		products.add(new Product("Tablet", 2400.0));
 		
-		List<String> names = products.stream().map(Product::toUpperCaseName).collect(Collectors.toList());
+		Function<Product, String> toUpperCase = (Product p) -> p.getName().toUpperCase();
+		
+		List<String> names = products.stream().map(toUpperCase).collect(Collectors.toList());
 		names.forEach(System.out::println);
 		
 	}
