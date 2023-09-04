04ad8d11c000187524b2450a3bc9b9f97c477fe7
Tiago Gomes <tidgomes@gmail.com>
2023-09-04 10:15:31 -0300

    apply map to a stream

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ import java.util.stream.Stream;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		List<Integer> list = Arrays.asList(3,4,5,7,8,11);
-		
-		Stream<Integer> st1 = list.stream();
+
+		List<Integer> list = Arrays.asList(3, 4, 5, 7, 8, 11);
+
+		Stream<Integer> st1 = list.stream().map(num -> num * 10);
 		System.out.println(Arrays.toString(st1.toArray()));
 
 	}
