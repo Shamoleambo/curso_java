000e9f6600041c24e37e8c7bd5cd4bfbf12b465a
Tiago Gomes <tidgomes@gmail.com>
2023-10-16 11:06:01 -0300

    let's go

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -7,9 +8,15 @@ public class Main {
 	public static void main(String[] args) {
 
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
-		
-		Stream<Integer> st1 = list.stream().map(x -> x *10);
+
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
 		System.out.println(Arrays.toString(st1.toArray()));
+
+		Integer sum = list.stream().reduce(0, (acc, elem) -> acc + elem);
+		System.out.println(sum);
+
+		List<Integer> newList = list.stream().filter(x -> x % 2 == 0).map(x -> x * 10).collect(Collectors.toList());
+		System.out.println(Arrays.toString(newList.toArray()));
 
 	}
 }
