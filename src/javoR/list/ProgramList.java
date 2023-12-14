6abf27328ed35379ff5aeb3f4138ed53dfbb438e
Tiago Gomes <tidgomes@gmail.com>
2023-12-14 14:58:23 -0300

    create Employee class

--- src/javoR/list/ProgramList.java
+++ src/javoR/list/ProgramList.java
@@ -1,4 +1,4 @@
-package javoR;
+package javoR.list;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ src/javorExs/list/Employee.java
@@ -0,0 +1,34 @@
+package javorExs.list;
+
+public class Employee {
+	private Integer id;
+	private String name;
+	private Double salary;
+
+	public Employee(Integer id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public void raiseSalary(Double percentAmount) {
+		double decimalAmount = percentAmount / 100;
+		this.salary *= (1 + decimalAmount);
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String newName) {
+		this.name = newName;
+	}
+
+	public Double getSalary() {
+		return this.salary;
+	}
+}
