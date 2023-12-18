00b9fe05e919974a89a95c2ceed0dd206ddcb667
Tiago Gomes <tidgomes@gmail.com>
2023-12-18 11:38:41 -0300

    learn how to parse dates in ISO 8601 format

--- src/javoR/dates/Program.java
+++ src/javoR/dates/Program.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		LocalDate d01 = LocalDate.now();
 		System.out.println("d01 = " + d01);
 		LocalDateTime d02 = LocalDateTime.now();
@@ -15,6 +15,14 @@ public class Program {
 		Instant d03 = Instant.now();
 		System.out.println("d03 = " + d03);
 
+		// ISO
+		LocalDate d04 = LocalDate.parse("2023-12-18");
+		System.out.println("d04 = " + d04);
+		LocalDateTime d05 = LocalDateTime.parse("2023-12-18T11:00");
+		System.out.println("d05 = " + d05);
+
+		Instant d06 = Instant.parse("2023-12-18T11:00:00-03:00");
+		System.out.println("d06 = " + d06);
 	}
 
 }
