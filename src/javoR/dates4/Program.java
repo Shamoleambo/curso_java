d0e8b3ec5f48a8f7145661d279cec8754d1774cb
Tiago Gomes <tidgomes@gmail.com>
2023-12-18 14:39:02 -0300

    modify time for instants

--- src/javoR/dates4/Program.java
+++ src/javoR/dates4/Program.java
@@ -1,26 +1,23 @@
 package javoR.dates4;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
 
-		LocalDateTime d01 = LocalDateTime.parse("2023-12-18T00:33:22");
-		LocalDateTime pastSecond = d01.minusSeconds(23);
-		LocalDateTime nextSecond = d01.plusSeconds(40);
-		LocalDateTime pastMinute = d01.minusMinutes(1);
-		LocalDateTime nextMinute = d01.plusMinutes(1);
-		LocalDateTime pastHour = d01.minusHours(1);
-		LocalDateTime nextHour = d01.plusHours(1);
+		Instant d01 = Instant.parse("2023-12-18T01:02:03Z");
+
+		Instant pastSecond = d01.minusSeconds(1);
+		Instant pastHour = d01.minus(2, ChronoUnit.HOURS);
+		Instant nextDay = d01.plus(1, ChronoUnit.DAYS);
 
 		System.out.println("pastSecond " + pastSecond);
-		System.out.println("nextSecond " + nextSecond);
-		System.out.println("pastMinute " + pastMinute);
-		System.out.println("nextMinute " + nextMinute);
 		System.out.println("pastHour " + pastHour);
-		System.out.println("nextHour " + nextHour);
+		System.out.println("nextDay " + nextDay);
+		
+		
 
 	}
 
