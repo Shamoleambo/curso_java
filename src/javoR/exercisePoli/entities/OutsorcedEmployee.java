2bb6cf9a83977c7fe6e70507a7a82cbc9e9ed8ec
Tiago Gomes <tidgomes@gmail.com>
2023-12-19 14:44:34 -0300

    create OutsourcedEmployee entity

---
+++ src/javoR/exercisePoli/entities/OutsorcedEmployee.java
@@ -0,0 +1,16 @@
+package javoR.exercisePoli.entities;
+
+public class OutsorcedEmployee extends Employee {
+	public Double additionalCharge;
+
+	public OutsorcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+
+	@Override
+	public Double payment() {
+		return super.payment() + 1.1 * additionalCharge;
+	}
+
+}
