5c0f56be3d156720c0c8d3c42739543964b5fe32
Tiago Gomes <tidgomes@gmail.com>
2024-03-13 10:03:31 -0300

    learn stack trace

--- src/s15tratamentoDeErros/Program.java
+++ src/s15tratamentoDeErros/Program.java
@@ -6,6 +6,19 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		method1();
+		System.out.println("End of program");
+
+	}
+
+	public static void method1() {
+		System.out.println("Method 1 start");
+		method2();
+		System.out.println("Method 1 end");
+	}
+
+	public static void method2() {
+		System.out.println("Method 2 start");
 		Scanner sc = new Scanner(System.in);
 
 		try {
@@ -14,11 +27,12 @@ public class Program {
 			System.out.println(vect[position]);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position");
+			e.printStackTrace();
 		} catch (InputMismatchException e) {
 			System.out.println("Invalid input");
 		}
 
-		System.out.println("End of program");
+		System.out.println("Method 2 end");
 		sc.close();
 
 	}
