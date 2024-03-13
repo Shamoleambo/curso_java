ce89584b62d9527eb119e89a8d63410572335074
Tiago Gomes <tidgomes@gmail.com>
2024-03-13 10:34:13 -0300

    learn how to open a file

---
+++ src/s17arquivos/Program.java
@@ -0,0 +1,25 @@
+package s17arquivos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		File file = new File("C:\\Users\\tidgo\\Documentos\\test.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine())
+				System.out.println(sc.nextLine());
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			if (sc != null)
+				sc.close();
+		}
+
+	}
+
+}
