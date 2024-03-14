5ba46df34e542cfc882af45ebb2171d20f423569
Tiago Gomes <tidgomes@gmail.com>
2024-03-14 19:47:50 -0300

    ultra learning achieved

--- src/s20functional/function/application/Program.java
+++ src/s20functional/function/application/Program.java
@@ -2,6 +2,7 @@ package s20functional.function.application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 import s20functional.function.entities.Product;
 
@@ -16,8 +17,10 @@ public class Program {
 		products.add(new Product("TV", 1500.0));
 		products.add(new Product("TV", 1500.0));
 		products.add(new Product("Smart Phone", 3400.0));
+		
+		Function<Product, String> func = p -> p.getName().toUpperCase();
 
-		List<String> names = products.stream().map(p -> p.getName().toUpperCase()).toList();
+		List<String> names = products.stream().map(func).toList();
 		names.forEach(System.out::println);
 
 	}
