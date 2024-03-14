02e5c45907b1ad1fb544d0c7eb17bc03a7651a31
Tiago Gomes <tidgomes@gmail.com>
2024-03-14 08:55:01 -0300

    create Installment model

---
+++ src/s18/contractEx/domain/Installment.java
@@ -0,0 +1,18 @@
+package s18.contractEx.domain;
+
+import java.time.LocalDate;
+
+public class Installment {
+	private LocalDate dueDate;
+	private Double amount;
+
+	public Installment(LocalDate dueDate, Double amount) {
+		this.dueDate = dueDate;
+		this.amount = amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+}
