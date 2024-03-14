fe56f4da5a7275156925220cc3a6efc1f63cb595
Tiago Gomes <tidgomes@gmail.com>
2024-03-14 09:16:52 -0300

    add more getters to Contract model

--- src/s18/contractEx/domain/Contract.java
+++ src/s18/contractEx/domain/Contract.java
@@ -1,11 +1,13 @@
 package s18.contractEx.domain;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Contract {
 	private Integer number;
 	private LocalDate date;
 	private Double value;
+	private ArrayList<Installment> installments;
 
 	public Contract(int number, LocalDate date, double value) {
 		this.number = number;
@@ -13,5 +15,16 @@ public class Contract {
 		this.value = value;
 	}
 	
-	
+	public Double getValue() {
+		return this.value;
+	}
+
+	public LocalDate getDate() {
+		return this.date;
+	}
+
+	public ArrayList<Installment> getInstallments() {
+		return this.installments;
+	}
+
 }
