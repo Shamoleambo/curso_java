b7d7cc8ffc125f23c3c4d2db33e56c8d4350799e
Tiago Gomes <tidgomes@gmail.com>
2024-03-14 09:06:07 -0300

    add a new own property to Contract

---
+++ src/s18/contractEx/service/ContractService.java
@@ -0,0 +1,11 @@
+package s18.contractEx.service;
+
+import contractInterface.entities.Contract;
+
+public class ContractService  {
+	private OnlinePaymentService onlinePaymentService;
+	
+	public void processContract(Contract contract, Integer months) {
+		
+	}
+}
