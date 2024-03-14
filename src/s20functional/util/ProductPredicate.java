ffcbfdb1463965c070021c225ecefad10a23513e
Tiago Gomes <tidgomes@gmail.com>
2024-03-14 18:18:18 -0300

    learn even more

--- src/s20functional/util/ProductPredicate.java
+++ src/s20functional/util/ProductPredicate.java
@@ -7,9 +7,6 @@ import s20functional.predicate.entities.Product;
 public class ProductPredicate implements Predicate<Product> {
 
 	public boolean test(Product p) {
-		if (p.getPrice() < 600)
-			return true;
-		else
-			return false;
+		return p.getPrice() < 600;
 	}
 }
