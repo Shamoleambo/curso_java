df06389957a607a34754a44f70109d05fd85d554
Tiago Gomes <tidgomes@gmail.com>
2024-03-14 09:17:45 -0300

    create ContractService class

--- src/s18/contractEx/service/ContractService.java
+++ src/s18/contractEx/service/ContractService.java
@@ -1,11 +1,23 @@
 package s18.contractEx.service;
 
-import contractInterface.entities.Contract;
+import java.time.LocalDate;
 
-public class ContractService  {
+import s18.contractEx.domain.Contract;
+import s18.contractEx.domain.Installment;
+
+public class ContractService {
 	private OnlinePaymentService onlinePaymentService;
-	
+
 	public void processContract(Contract contract, Integer months) {
-		
+		for (int i = 1; i <= months; i++) {
+			LocalDate installmentDate = contract.getDate().plusMonths(i);
+			Double amount = contract.getValue();
+
+			Double paymentFee = onlinePaymentService.paymentFee(amount);
+			Double interest = onlinePaymentService.interest(amount, i);
+			amount = amount + paymentFee + interest;
+
+			contract.getInstallments().add(new Installment(installmentDate, amount));
+		}
 	}
 }
